2fecc531c921d6ffc7f6646445893f0f42a978c5
gseller99 <33374369+gseller99@users.noreply.github.com>
2018-01-11 19:54:10 -0600

    2018 01 11

---
+++ app.js
@@ -0,0 +1,22 @@
+// const myHeading = document.getElementById('myHeading');
+// const myButton = document.getElementById('myButton');
+// const myTextInput = document.getElementById('myTextInput');
+// const resetToBlack = document.getElementById('resetToBlack');
+
+// myButton.addEventListener('click', () => {
+// 	myHeading.style.color = myTextInput.value;
+// });
+
+// resetToBlack.addEventListener('click', () => {
+// 	myHeading.style.color = 'purple';
+// });
+function randomNumber(upper) {
+	return Math.floor (Math.random() * upper) + 1;
+}
+
+var counter = 0;
+while (counter < 1000) {
+	var randNum = randomNumber(6);
+	document.write('<p> '+ randNum + ' </p>');
+	counter += 1;
+};
